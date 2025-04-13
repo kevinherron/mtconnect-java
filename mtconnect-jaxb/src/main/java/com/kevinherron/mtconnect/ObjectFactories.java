64f356ebedd63005f01bc1826de62d76b2463cbf
Kevin Herron <kevinherron@gmail.com>
2025-04-13 08:16:17 -0700

    Fix visibility of static fields in `ObjectFactories`

--- mtconnect-jaxb/src/main/java/com/kevinherron/mtconnect/ObjectFactories.java
+++ mtconnect-jaxb/src/main/java/com/kevinherron/mtconnect/ObjectFactories.java
@@ -2,11 +2,13 @@ package com.kevinherron.mtconnect;
 
 public class ObjectFactories {
 
-  static final Class<?> ASSETS_OBJECT_FACTORY = org.mtconnect.v20.assets.ObjectFactory.class;
+  public static final Class<?> ASSETS_OBJECT_FACTORY = org.mtconnect.v20.assets.ObjectFactory.class;
 
-  static final Class<?> DEVICES_OBJECT_FACTORY = org.mtconnect.v20.devices.ObjectFactory.class;
+  public static final Class<?> DEVICES_OBJECT_FACTORY =
+      org.mtconnect.v20.devices.ObjectFactory.class;
 
-  static final Class<?> ERROR_OBJECT_FACTORY = org.mtconnect.v20.error.ObjectFactory.class;
+  public static final Class<?> ERROR_OBJECT_FACTORY = org.mtconnect.v20.error.ObjectFactory.class;
 
-  static final Class<?> STREAMS_OBJECT_FACTORY = org.mtconnect.v20.streams.ObjectFactory.class;
+  public static final Class<?> STREAMS_OBJECT_FACTORY =
+      org.mtconnect.v20.streams.ObjectFactory.class;
 }
